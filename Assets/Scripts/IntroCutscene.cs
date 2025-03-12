2dc9c377367984b4a17a60cf08b4e9b85cf2ed2c
ssotoale <52551359+ssotoale@users.noreply.github.com>
2025-03-11 21:31:46 -0700

    testing
    changed location of script

--- Assets/Scripts/IntroCutscene.cs
+++ Assets/Scripts/IntroCutscene.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class IntroCutscene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // testing
     void OnEnable()
     {
         SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
